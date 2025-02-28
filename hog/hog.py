e87cae8e1ba7741c385e0b49ffb22300efd8faaf
Tategoto Azarasi <2724167997@qq.com>
2025-02-28 20:50:28 +0800

    hog problem 10
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- hog/hog.py
+++ hog/hog.py
@@ -344,6 +344,9 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
 	points, and returns NUM_ROLLS otherwise. Ignore the Sus Fuss rule.
 	"""
 	# BEGIN PROBLEM 10
+	bb_score = boar_brawl(score, opponent_score)
+	if bb_score >= threshold:
+		return 0
 	return num_rolls  # Remove this line once implemented.
 
 
