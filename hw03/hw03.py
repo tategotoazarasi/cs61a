05f942fc2f62b39135ddd62e8d0074c2e8579f93
Tategoto Azarasi <2724167997@qq.com>
2025-03-11 17:07:03 +0800

    hw03 Q1: Num Eights
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- hw03/hw03.py
+++ hw03/hw03.py
@@ -24,7 +24,12 @@ def num_eights(n):
 	...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
 	True
 	"""
-	"*** YOUR CODE HERE ***"
+	if n == 0:
+		return 0
+	if n % 10 == 8:
+		return 1 + num_eights(n // 10)
+	else:
+		return num_eights(n // 10)
 
 
 def digit_distance(n):
